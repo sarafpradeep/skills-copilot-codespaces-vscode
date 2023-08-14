d501701b3470c25cc7fa044ae85467da3454c0ed
PSaraf <33158438+sarafpradeep@users.noreply.github.com>
2023-08-14 00:52:46 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,10 @@
+function skillsMember() {
+    var member = document.getElementById("member");
+    var skills = document.getElementById("skills");
+    var memberBtn = document.getElementById("memberBtn");
+    var skillsBtn = document.getElementById("skillsBtn");
+    member.style.display = "block";
+    skills.style.display = "none";
+    memberBtn.style.backgroundColor = "#f9f9f9";
+    skillsBtn.style.backgroundColor = "#fff";
+}
